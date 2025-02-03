30b27702dce4e7f3250c7d35a3da9e4f09d1a7aa
Andrey Zashchepin <razlivnoy@icloud.com>
2025-02-03 17:25:58 +0500

    Прописана админка

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name',)
+    search_fields = ('category_name', 'description',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('product_name', 'description',)
