86d7b3382c8dbeb2c6736d3705bfd174e52b36f6
Andrey Zashchepin <razlivnoy@icloud.com>
2025-02-16 23:37:40 +0500

    Пагинация

--- catalog/views.py
+++ catalog/views.py
@@ -10,6 +10,7 @@ class ProductListView(ListView):
     model = Product
     template_name = 'catalog/products_list.html'
     context_object_name = 'products'
+    paginate_by = 4
 
 
 class ProductDetailView(DetailView):
