64535eaa572bdd295b37aecbdaaa5fbd2b5862c3
Andrey Zashchepin <razlivnoy@icloud.com>
2025-02-27 19:29:39 +0500

    Валидация загрузки изображения

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,5 +1,3 @@
-import logging
-
 from django import forms
 from django.core.exceptions import ValidationError
 from django.db.models import BooleanField
@@ -7,7 +5,6 @@ from django.utils.safestring import mark_safe
 
 from catalog.models import Product
 
-logger = logging.getLogger(__name__)
 
 FORBIDDEN_WORDS = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар', ]
 
@@ -45,39 +42,42 @@ class ProductForm(StyleFormMixin, forms.ModelForm):
 
     def clean_product_name(self):
         product_name = self.cleaned_data.get('product_name')
-        logger.debug(f'Валидное название продукта: {product_name}')
         if product_name:
             for word in FORBIDDEN_WORDS:
                 if word in product_name.lower():
-                    logger.error(f'Обнаружено запрещенное слово: {word}')
                     raise ValidationError(f'Недопустимо использовать "{word}" в названии продукта')
         else:
-            logger.error('Название продукта отсутствует или пустое')
             raise ValidationError('Название продукта обязательно для заполнения.')
         return product_name
 
     def clean_description(self):
         description = self.cleaned_data.get('description')
-        logger.debug(f'Валидное описание продукта: {description}')
         if description:
             for word in FORBIDDEN_WORDS:
                 if word in description.lower():
-                    logger.error(f'Обнаружено запрещенное слово: {word}')
                     raise ValidationError(f'Недопустимо использовать "{word}" в описании продукта')
         else:
-            logger.error('Описание продукта отсутствует или пустое')
             raise ValidationError('Описание продукта обязательно для заполнения.')
         return description
 
+    def clean_image(self):
+        image = self.cleaned_data.get('image')
+        if image:
+            valid_formats = ['image/jpeg', 'image/png']
+            if image.content_type not in valid_formats:
+                raise ValidationError('Формат файла должен быть JPEG или PNG.')
+
+            max_size = 5 * 1024 * 1024
+            if image.size > max_size:
+                raise ValidationError('Размер файла не должен превышать 5 МБ.')
+        return image
+
     def clean_price(self):
         price = self.cleaned_data.get('price')
-        logger.debug(f'Валидная стоимость продукта: {price}')
         if price:
             if price < 0:
-                logger.error('Введена отрицательная стоимость продукта')
                 raise ValidationError('Стоимость продукта не может быть меньше нуля')
         else:
-            logger.debug('Стоимость продукта отсутствует')
             raise ValidationError('Стоимость продукта обязательна для заполнения.')
         return price
 
@@ -86,12 +86,9 @@ class ProductForm(StyleFormMixin, forms.ModelForm):
         product_name = cleaned_data.get('product_name')
         description = cleaned_data.get('description')
 
-        logger.debug(f'Очищенные данные: {cleaned_data}')
         if not product_name:
-            logger.error('Поле product_name отсутствует или пустое')
             raise ValidationError({'product_name': 'Это поле обязательно для заполнения.'})
         if not description:
-            logger.error('Поле description отсутствует или пустое')
             raise ValidationError({'description': 'Это поле обязательно для заполнения.'})
 
         return cleaned_data
