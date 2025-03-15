197bbc0a1f9eb1dec199f3ecbc93a758427e4245
Andrey Zashchepin <razlivnoy@icloud.com>
2025-03-15 17:51:39 +0500

    Правка регистрации и отправки писем

--- users/views.py
+++ users/views.py
@@ -1,14 +1,49 @@
 import secrets
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.shortcuts import get_object_or_404, redirect
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy, reverse
+from django.utils.html import strip_tags
 from django.views.generic import CreateView, UpdateView
 
 from config.settings import DEFAULT_FROM_EMAIL
 from users.forms import CustomUserCreationForm, CustomUserChangeForm
 from users.models import CustomUser
+
+
+def send_verification_email(user_email, host, url):
+    subject = f'Подтверждение почты на сайте {host}'
+    html_message = render_to_string('users/verification_email.html', {
+        'host': host,
+        'url': url,
+    })
+    plain_message = strip_tags(html_message)
+
+    email = EmailMultiAlternatives(
+        subject=subject,
+        body=plain_message,
+        from_email=DEFAULT_FROM_EMAIL,
+        to=[user_email],
+    )
+    email.attach_alternative(html_message, "text/html")
+    email.send()
+
+
+def send_welcome_email(user_email):
+    subject = 'Добро пожаловать на наш сайт!'
+    html_message = render_to_string('users/welcome_email.html')
+    plain_message = strip_tags(html_message)
+
+    email = EmailMultiAlternatives(
+        subject=subject,
+        body=plain_message,
+        from_email=DEFAULT_FROM_EMAIL,
+        to=[user_email],
+    )
+    email.attach_alternative(html_message, "text/html")
+    email.send()
 
 
 class RegisterView(CreateView):
@@ -24,26 +59,16 @@ class RegisterView(CreateView):
         user.save()
         host = self.request.get_host()
         url = f'http://{host}/users/confirm/{token}/'
-        self.send_verification_email(user.email, host, url)
+        send_verification_email(user.email, host, url)
         return super().form_valid(form)
 
-    def send_verification_email(self, user_email, host, url):
-        send_mail(
-            subject=f'Подтверждение почты на сайте {host}',
-            message=f'''Здравствуйте!
 
-                    Для завершения регистрации на сайте http://{host}/, пройдите по ссылке: 
-                    {url} 
-                    и подтвердите ваш email.''',
-            from_email=DEFAULT_FROM_EMAIL,
-            recipient_list=[user_email]
-        )
-
-
-def email_verification(request, token):
+def email_verification(token):
     user = get_object_or_404(CustomUser, token=token)
     user.is_active = True
     user.save()
+
+    send_welcome_email(user.email)
     return redirect(reverse('users:login'))
 
 
